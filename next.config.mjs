b8c74ca6581794e32b21d2304f66cbb7c19b0c84
ru jia <123100909@umail.ucc.ie>
2025-10-28 17:46:28 +0000

    chore(next): update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images-assets.nasa.gov' }, // Image Library 资源
-      { protocol: 'https', hostname: 'images-api.nasa.gov' },    // 缩略图等（偶尔）
-      { protocol: 'https', hostname: 'epic.gsfc.nasa.gov' },     // EPIC（若用）
-      { protocol: 'https', hostname: 'apod.nasa.gov' }           // 兼容历史/APOD
+      { protocol: 'https', hostname: 'images-assets.nasa.gov' }, 
+      { protocol: 'https', hostname: 'images-api.nasa.gov' },    
+      { protocol: 'https', hostname: 'epic.gsfc.nasa.gov' },     
+      { protocol: 'https', hostname: 'apod.nasa.gov' }           
     ],
   },
 };
